4a23cbaedd27bd379b81172aca18361292e13020
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2023-01-10 22:19:16 +0300

    Add files via upload

---
+++ files/js/contact-form.js
@@ -0,0 +1,76 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+
+   // Контактная форма
+   const contact_form = document.getElementById('contact-form');
+   contact_form.addEventListener('submit', formSend);
+
+   async function formSend(e) {
+      e.preventDefault();
+
+      let error = formValidate(contact_form);
+      let formData = new FormData(contact_form);
+
+      if (error === 0) {
+         contact_form.classList.add('_sending');
+
+         let response = await fetch('/files/phpmailer/sendmail.php', {
+            method: 'POST',
+            body: formData
+         });
+         if (response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            contact_form.reset();
+            contact_form.classList.remove('_sending');
+         } else {
+            alert('Ошибка');
+            contact_form.classList.remove('_sending');
+         }
+      } else {
+         //alert('Заполните обязательные поля');
+      }
+   }
+
+   function formValidate(contact_form) {
+      let error = 0;
+      let formReq = document.querySelectorAll('._req');
+
+      for (let index = 0; index < formReq.length; index++) {
+         const input = formReq[index];
+         formRemoveError(input);
+
+         if (input.classList.contains('_email')) {
+            if (emailTest(input)) {
+               if (input.value === '') {
+                  formAddError(input);
+                  error++;
+               } else {
+                  formAddError(input);
+               }
+            }
+         } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+            formAddError(input);
+            error++;
+         } else {
+            if (input.value === '') {
+               formAddError(input);
+               error++;
+            }
+         }
+      }
+      return error;
+   }
+   function formAddError(input) {
+      input.parentElement.classList.add('_required');
+      input.classList.add('_required');
+   }
+   function formRemoveError(input) {
+      input.parentElement.classList.remove('_required');
+      input.classList.remove('_required');
+   }
+   function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+   }
+});
